ffb84ec5e22f31fd8a7a8da6f1f3d151c92cc683
omergouner1 <143837147+omergouner1@users.noreply.github.com>
2024-10-31 10:39:29 -0500

    Update WindowsCleanerUtility.bat

--- WindowsCleanerUtility.bat
+++ WindowsCleanerUtility.bat
@@ -3,15 +3,6 @@ mode con cols=122 lines=40
 chcp 65001
 cls
 
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
